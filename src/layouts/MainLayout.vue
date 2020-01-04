dee4f6a591f6b6bc8e3ca9548669829efcb8f2a8
Tavisco <tavisco@ymail.com>
2020-01-04 16:30:45 -0300

    feat(Main Layout): Removes default texts and changes toolbar color

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,98 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated>
+      <q-toolbar class="bg-toolbar">
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          icon="menu"
+          aria-label="Menu"
+        />
+
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="~/assets/bolt.svg" />
+          </q-avatar>
+
+          Pickup Store
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-2"
+    >
+      <q-list>
+        <q-item-label header>Menu</q-item-label>
+
+        <q-item clickable tag="a" target="_blank" href="https://quasar.dev">
+          <q-item-section avatar>
+            <q-icon name="home" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Home</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item
+          clickable
+          tag="a"
+          target="_blank"
+          href="https://github.quasar.dev"
+        >
+          <q-item-section avatar>
+            <q-icon name="directions_car" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Cars</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item
+          clickable
+          tag="a"
+          target="_blank"
+          href="https://chat.quasar.dev"
+        >
+          <q-item-section avatar>
+            <q-icon name="map" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Tracks</q-item-label>
+          </q-item-section>
+        </q-item>
+
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MainLayout',
+
+  data () {
+    return {
+      leftDrawerOpen: false
+    }
+  }
+}
+</script>
+
+<style>
+
+.bg-toolbar {
+  background: #4b4b4b;
+}
+
+</style>
