50c00e786357cd5e7b63a4a272dc14a7c7db33ac
Tavisco <Tavisco@users.noreply.github.com>
2020-02-25 22:37:45 -0300

    feat(home): basic homescreen added

---
+++ src/pages/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-page>
+    <h3 class="text-weight-light">Home</h3>
+
+    <div class="q-pa-md q-gutter-sm row">
+
+      <q-banner rounded class="bg-grey-3 col-3">
+        <template v-slot:avatar>
+          <q-icon name="star" color="black" />
+        </template>
+        <strong>
+        Not much to see here for now. <br />
+        </strong>
+        Try using the menu on the left!
+      </q-banner>
+
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageHome'
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 15px;
+}
+</style>
