5f79175e52a4c04cfd1f944fcf06609405341324
Tavisco <Tavisco@users.noreply.github.com>
2020-03-05 19:34:29 -0300

    feat(cars): basic cars screen layout

--- src/pages/Cars.vue
+++ src/pages/Cars.vue
@@ -3,6 +3,13 @@
     <!-- content -->
     <h3 class="text-weight-light">Cars</h3>
 
+    <div class="q-pa-md q-gutter-y-md column items-start">
+      <q-btn-group outline>
+        <q-btn outline color="primary" label="Publish new car" icon="library_add" />
+        <q-btn outline color="secondary" label="Manage" icon="library_books" />
+      </q-btn-group>
+    </div>
+
     <q-card flat bordered class="latest-cars-card">
       <q-card-section>
         <div class="text-h6">Latest cars</div>
@@ -14,8 +21,11 @@
               src="https://cdn.quasar.dev/img/parallax2.jpg"
               :ratio="1"
             >
-              <div class="absolute-bottom text-h6">
-                {{car.name}}
+              <div class="absolute-bottom">
+                <div class=" text-h6">
+                  {{car.name}}
+                </div>
+                <div class="text-subtitle2">by John Doe</div>
               </div>
             </q-img>
           </q-card>
@@ -23,6 +33,52 @@
 
     </q-card>
 
+    <div class="q-pa-md">
+      <q-table
+        title="Cars"
+        :data="data"
+        :columns="columns"
+        row-key="name"
+        :filter="filter"
+        grid
+        hide-header
+      >
+        <template v-slot:top-right>
+          <q-input outlined dense debounce="300" v-model="filter" placeholder="Search">
+            <template v-slot:append>
+              <q-icon name="search" />
+            </template>
+          </q-input>
+        </template>
+
+        <template v-slot:item="props">
+          <div
+            class="q-pa-xs col-xs-6 col-sm-6 col-md-4 col-lg-3 grid-style-transition"
+          >
+          <q-responsive :ratio="1" class="col">
+            <q-card>
+              <q-card-section>
+                <!-- <q-checkbox dense v-model="props.selected" :label="props.row.name" /> -->
+                <div class="text-h6 text-weight-light text-no-wrap" style="overflow-x: auto">{{ props.row.name }}</div>
+              </q-card-section>
+              <q-separator />
+              <q-list dense>
+                <q-item v-for="col in props.cols.filter(col => col.name !== 'desc')" :key="col.name">
+                  <q-item-section>
+                    <q-item-label>{{ col.label }}</q-item-label>
+                  </q-item-section>
+                  <q-item-section side>
+                    <q-item-label caption>{{ col.value }}</q-item-label>
+                  </q-item-section>
+                </q-item>
+              </q-list>
+            </q-card>
+          </q-responsive>
+          </div>
+        </template>
+
+      </q-table>
+    </div>
   </q-page>
 </template>
 
@@ -62,7 +118,55 @@ export default {
         {
           name: 'Car 10'
         }
+      ],
+      filter: '',
+      columns: [
+        {
+          name: 'desc',
+          required: true,
+          label: 'Dessert (100g serving)',
+          align: 'left',
+          field: row => row.name,
+          format: val => `${val}`,
+          sortable: true
+        },
+        { name: 'calories', align: 'center', label: 'Calories', field: 'calories', sortable: true },
+        { name: 'fat', label: 'Fat (g)', field: 'fat', sortable: true },
+        { name: 'carbs', label: 'Carbs (g)', field: 'carbs' }
+      ],
+      data: [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24
+        },
+        {
+          name: 'Ice cream sandwich',
+          calories: 237,
+          fat: 9.0,
+          carbs: 37
+        },
+        {
+          name: 'Eclair',
+          calories: 262,
+          fat: 16.0,
+          carbs: 23
+        },
+        {
+          name: 'Cupcake',
+          calories: 305,
+          fat: 3.7,
+          carbs: 67
+        },
+        {
+          name: 'Gingerbread',
+          calories: 356,
+          fat: 16.0,
+          carbs: 49
+        }
       ]
+
     }
   }
 }
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,18 +2,18 @@
   <q-page>
     <h3 class="text-weight-light">Home</h3>
 
-    <div class="q-pa-md q-gutter-sm row">
-
-      <q-banner rounded class="bg-grey-3 col-3">
-        <template v-slot:avatar>
-          <q-icon name="star" color="black" />
-        </template>
-        <strong>
-        Not much to see here for now. <br />
-        </strong>
-        Try using the menu on the left!
-      </q-banner>
-
+    <div class="q-pa-md row">
+      <div class="col-md-3 col-sm-12">
+        <q-banner rounded class="bg-grey-3">
+          <template v-slot:avatar>
+            <q-icon name="star" color="black" />
+          </template>
+          <strong>
+          Not much to see here for now. <br />
+          </strong>
+          Try using the menu on the left!
+        </q-banner>
+      </div>
     </div>
   </q-page>
 </template>
