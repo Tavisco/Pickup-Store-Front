3ad9840d2f3f13c4b55d4ccba27ef528cf18cf3a
Tavisco <otavio.pinheiro@mercadolivre.com>
2020-10-20 18:06:22 -0300

    Starts implementing new car wizard

--- src/pages/cars/NewCar.vue
+++ src/pages/cars/NewCar.vue
@@ -3,89 +3,123 @@
     <h3 class="text-weight-light">Submit a new car</h3>
     <!-- content -->
     <div class="row justify-center">
-      <q-card class="col-md-6 " flat bordered>
-        <q-card-section>
-          <div class="text-h6">Car details</div>
-        </q-card-section>
 
-        <q-card-section>
-          <q-form
-            @submit="onSubmit"
-            @reset="onReset"
-            class="q-gutter-md"
+      <q-banner
+        inline-actions
+        rounded
+        class="warn-banner text-white bg-red"
+        v-if="error"
+      >
+        <strong>Oops! Something went wrong!</strong> {{this.error}}
+        <template v-slot:action>
+          <q-btn flat color="white" label="Dismiss" @click="error = ''"/>
+        </template>
+      </q-banner>
+
+      <div class="col-md-6">
+        <q-stepper
+          v-model="step"
+          ref="stepper"
+          color="primary"
+          animated
+        >
+          <q-step
+            :name="1"
+            title="Basic information"
+            icon="settings"
+            :done="step > 1"
           >
-            <q-input
-              filled
-              v-model="name"
-              label="Car name *"
-              hint="ie: Dr. Grudge"
-              lazy-rules
-              :rules="[ val => val && val.length > 0 || 'Please type something']"
-            />
-
-            <q-input
-              v-model="description"
-              label="Car description *"
-              hint="ie: Dr. Grudge is a stock Re-Volt car"
-              filled
-              autogrow
-              lazy-rules
-              :rules="[ val => val && val.length > 0 || 'Please type something']"
-            />
-
-            <q-file
-              v-model="file"
-              label="Pick car .zip file"
-              filled
-              counter
-              :counter-label="counterLabelFn"
-              max-files="1"
-              accept=".zip"
-              style="width: 97%"
-              lazy-rules
-              :rules="[ val => val || 'Please select a file']"
-            >
-              <template v-slot:prepend>
-                <q-icon name="attach_file" />
-              </template>
-              <template v-if="file" v-slot:append>
-                <q-icon name="cancel" @click.stop.prevent="file = null" class="cursor-pointer" />
-              </template>
-            </q-file>
-
-            <div v-for="(author, index) in authors" v-bind:key="index">
-              <q-input
-                v-model="author.name"
-                :label="'Car author ' + (index+1) + ' *'"
-                hint="ie: Acclaim"
-                filled
-                lazy-rules
-                :rules="[ val => val && val.length > 0 || 'Please type something']"
-                bottom-slots
+            <q-form
+                class="q-gutter-md"
+                ref="basicInfoForm"
               >
-              <template v-slot:append>
-                <q-icon v-if="index !== 0" name="delete_forever" class="cursor-pointer" @click="authors.splice(index, 1)" />
-              </template>
-              <template v-slot:after>
-                <q-icon v-if="index === authors.length-1 && author.name !== null" name="add" class="cursor-pointer" @click="addAuthor()" />
-              </template>
-              </q-input>
-            </div>
+                <q-input
+                  filled
+                  v-model="name"
+                  label="Car name *"
+                  hint="ie: Dr. Grudge"
+                  lazy-rules
+                  :rules="[ val => val && val.length > 0 || 'Please type something']"
+                />
 
-            <q-separator inset />
+                <q-input
+                  v-model="description"
+                  label="Car description *"
+                  hint="ie: Dr. Grudge is a stock Re-Volt car"
+                  filled
+                  autogrow
+                  lazy-rules
+                  :rules="[ val => val && val.length > 0 || 'Please type something']"
+                />
 
-            <q-card-actions align="right">
-              <q-btn flat label="Reset" type="reset" color="primary" class="q-ml-sm" />
-              <q-btn :loading="uploading" label="Submit" type="submit" color="primary" style="width: 150px">
-                <template v-slot:loading>
-                  <q-spinner-hourglass class="on-left" />
-                  Uploading...
-                </template>
-              </q-btn>
-            </q-card-actions>
-          </q-form>
-        </q-card-section>
-      </q-card>
+                <!-- <q-file
+                  v-model="file"
+                  label="Pick car .zip file"
+                  filled
+                  counter
+                  :counter-label="counterLabelFn"
+                  max-files="1"
+                  accept=".zip"
+                  style="width: 97%"
+                  lazy-rules
+                  :rules="[ val => val || 'Please select a file']"
+                >
+                  <template v-slot:prepend>
+                    <q-icon name="attach_file" />
+                  </template>
+                  <template v-if="file" v-slot:append>
+                    <q-icon name="cancel" @click.stop.prevent="file = null" class="cursor-pointer" />
+                  </template>
+                </q-file> -->
+
+                <div v-for="(author, index) in authors" v-bind:key="index">
+                  <q-input
+                    v-model="author.name"
+                    :label="'Car author ' + (index+1) + ' *'"
+                    hint="ie: Acclaim"
+                    filled
+                    lazy-rules
+                    :rules="[ val => val && val.length > 0 || 'Please type something']"
+                    bottom-slots
+                  >
+                  <template v-slot:append>
+                    <q-icon v-if="index !== 0" name="delete_forever" class="cursor-pointer" @click="authors.splice(index, 1)" />
+                  </template>
+                  <template v-slot:after>
+                    <q-icon v-if="index === authors.length-1 && author.name !== null" name="add" class="cursor-pointer" @click="addAuthor()" />
+                  </template>
+                  </q-input>
+                </div>
+              </q-form>
+          </q-step>
+
+          <q-step
+            :name="2"
+            title="Upload zip file"
+            icon="cloud_upload"
+            :done="step > 2"
+          >
+            An ad group contains one or more ads which target a shared set of keywords.
+          </q-step>
+
+          <q-step
+            :name="3"
+            title="Review"
+            icon="assignment"
+          >
+            This step won't show up because it is disabled.
+          </q-step>
+
+          <template v-slot:navigation>
+            <q-stepper-navigation>
+              <q-btn v-if="step === 1" flat color="primary" @click="onReset()" label="Reset" class="q-ml-sm"/>
+              <q-btn @click="onNextStep(step)" color="primary" :label="getLabel(step)" />
+              <q-btn v-if="step > 1" flat color="primary" @click="$refs.stepper.previous()" label="Back" class="q-ml-sm" />
+            </q-stepper-navigation>
+          </template>
+        </q-stepper>
+      </div>
+
     </div>
   </q-page>
 </template>
@@ -101,7 +135,10 @@ export default {
       description: null,
       authors: [],
       file: null,
-      uploading: false
+      uploading: false,
+      step: 1,
+      error: '',
+      errorVisible: false
     }
   },
   created () {
@@ -111,28 +148,10 @@ export default {
       }
     ]
   },
+
   methods: {
     async onSubmit () {
       this.uploading = true
-
-      // this.$q.notify({
-      //   color: 'green-4',
-      //   textColor: 'white',
-      //   icon: 'cloud_done',
-      //   message: 'Submitted'
-      // })
-      /*
-          Initialize the form data
-      */
-      let formData = new FormData()
-
-      /*
-          Add the form data we need to submit
-      */
-      formData.append('file', this.file)
-      formData.append('name', this.name)
-      formData.append('description', this.description)
-      formData.append('authors', this.authors)
 
       /*
         Make the request to the POST /single-file URL
@@ -140,17 +159,14 @@ export default {
       let accessToken = await this.$auth.getAccessToken()
 
       axios.post(process.env.CARS_API + '/cars',
-        formData,
+        this.uploading,
         {
           headers: {
-            'Content-Type': 'multipart/form-data',
             'Authorization': `Bearer ${accessToken}`
           }
-        }).then(function () {
-        console.log('SUCCESS!!')
-      }).catch(function () {
-        console.log('FAILURE!!')
-      })
+        })
+        .then(response => { this.$refs.stepper.next() })
+        .catch(error => this.handleError(error))
     },
 
     onReset () {
@@ -162,6 +178,7 @@ export default {
         }
       ]
       this.file = null
+      this.$refs.basicInfoForm.resetValidation()
     },
 
     addAuthor () {
@@ -170,9 +187,49 @@ export default {
           name: null
         })
     },
+
     counterLabelFn ({ totalSize, filesNumber, maxFiles }) {
       return `${filesNumber} file of ${maxFiles} | ${totalSize}`
+    },
+
+    getLabel (step) {
+      switch (step) {
+        case 1:
+          if (this.uploading) {
+            return 'Uploading...'
+          } else {
+            return 'Save and continue'
+          }
+        case 3:
+          return 'Finish'
+
+        default:
+          return 'Continue'
+      }
+    },
+
+    onNextStep (step) {
+      if (step === 1) {
+        this.$refs.basicInfoForm.validate().then(success => {
+          if (success) {
+            this.onSubmit()
+          }
+        })
+      }
+
+      // this.$refs.stepper.next()
+    },
+
+    handleError (error) {
+      console.log('Failure!!! ' + error)
+      this.error = error
+      this.uploading = false
     }
   }
 }
 </script>
+
+<style lang="sass" scoped>
+  .warn-banner
+    margin-bottom: 20px;
+</style>
